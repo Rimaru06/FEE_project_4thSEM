a93919da8de8494f0fe333b2a732679fee617088
Rimaru06 <shivraodanger04@gmail.com>
2024-02-28 18:27:51 +0530

    work out section completed

--- fetch/script.js
+++ fetch/script.js
@@ -2,47 +2,50 @@ const params = new URLSearchParams(window.location.search);
 const param = params.get('name');
 const api = document.getElementById('api');
 async function excersie(name) {
-    const url = `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${name}?limit=24`;
+    const url = `https://work-out-api1.p.rapidapi.com/search?Muscles=${name}`;
     const options = {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': 'b8eadd59dfmsh1a0d3a9f1c50669p151dedjsn0b366526076b',
-            'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+            'X-RapidAPI-Host': 'work-out-api1.p.rapidapi.com'
         }
     };
 
     try {
         const response = await fetch(url, options);
         const result = await response.json();
-        console.log(result)
-
+        const {"Beginner Sets" : sets} = result;
+        console.log(result);
         result.map((item) => {
             api.innerHTML += `
-            <div class="container">
-                <div class="img" style="background-image: url(${item.gifUrl});">
-                    <div class="img-details">
-                        <ul>
-                            <li> <span class="title">${item.bodyPart}</span> </li>
-                            <li> <span class="equipement">equipement : ${item.equipment}</span> </li>
-                            <li> <span class="targetMusle">Secondary Muscle : ${item.secondaryMuscles}</span> </li>
-                        </ul>
-                        <h1>instructions : </h1>
-                        <ul>
-                            <li> <span >1. ${item.instructions[0]}</span> </li>
-                            <li> <span >2. ${item.instructions[1]}</span> </li>
-                            <li> <span >3. ${item.instructions[2]}</span> </li>
-                            <li> <span >4. ${item.instructions[3]}</span> </li>
-                            <li> <span >5. ${item.instructions[4]}</span> </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="info">
-                    <div class="challange">
-                        <span><h4>Name : ${item.name}</h4></span>
-                        <span><h4>Target : ${item.target}</h4></span>
-                    </div>
-                </div>
-            </div> `;
+
+<div class="container">
+  <div>
+    <h1>${item.WorkOut} : ${item.Intensity_Level}</h1>
+  </div>
+  <hr />
+  <div class="sets">
+    <div>
+      <h3>Beginner sets :</h3>
+      <p>${item["Beginner Sets"]}</p>
+    </div>
+    <div>
+      <h3>Expert sets :</h3>
+      <p>${item["Expert Sets"]}</p>
+    </div>
+  </div>
+  <hr>
+  <div class="about">
+    <h2>About :</h2>
+    <p>${item["Long Explanation"]}</p>
+  </div>
+  <hr>
+  <div class="video">
+    <a href="item.Video"><button>Watch Video</button></a>
+  </div>
+</div>
+
+             `;
         }); 
     } catch (error) {
         console.error(error);
--- fetch/workout.js
+++ fetch/workout.js
@@ -1,18 +1,37 @@
 const imgdiv = document.getElementById('back');
 if (imgdiv) {
     imgdiv.addEventListener('click', function () {
-        window.location.href = 'http://192.168.86.135:5500/fetch/index.html?name=back';
+        window.location.href = 'http://localhost:63342/Fee_Project/fetch/index.html?name=Back'
     });
 }
 const imgdiv1 = document.getElementById('chest');
 if (imgdiv1) {
     imgdiv1.addEventListener('click', function () {
-        window.location.href = 'http://192.168.86.135:5500/fetch/index.html?name=chest';
+        window.location.href = 'http://localhost:63342/Fee_Project/fetch/index.html?name=Chest';
     });
 }
 const imgdiv2 = document.getElementById('legs');
 if (imgdiv2) {
     imgdiv2.addEventListener('click', function () {
-        window.location.href = 'http://192.168.86.135:5500/fetch/index.html?name=lower%20legs';
+        window.location.href = 'http://localhost:63342/Fee_Project/fetch/index.html?name=Legs';
     });
 }
+const imgdiv3 = document.getElementById('bicep');
+if (imgdiv3) {
+    imgdiv3.addEventListener('click', function () {
+        window.location.href = 'http://localhost:63342/Fee_Project/fetch/index.html?name=Bicep';
+    });
+}
+const imgdiv4 = document.getElementById('shoulder');
+if (imgdiv4) {
+    imgdiv4.addEventListener('click', function () {
+        window.location.href = 'http://localhost:63342/Fee_Project/fetch/index.html?name=Shoulder';
+    });
+}
+const imgdiv5 = document.getElementById('tricep');
+if (imgdiv5) {
+    imgdiv5.addEventListener('click', function () {
+        window.location.href = 'http://localhost:63342/Fee_Project/fetch/index.html?name=Tricep';
+    });
+}
+
