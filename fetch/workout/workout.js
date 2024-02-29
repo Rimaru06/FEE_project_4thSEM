d4f3b38ea112323885abeb197a3a0c2cfe4459d9
Rimaru06 <shivraodanger04@gmail.com>
2024-02-29 20:43:09 +0530

    workout to index

--- fetch/workout/workout.js
+++ fetch/workout/workout.js
@@ -1,37 +1,37 @@
 const imgdiv = document.getElementById('back');
 if (imgdiv) {
     imgdiv.addEventListener('click', function () {
-        window.location.href = 'http://localhost:63342/Fee_Project/fetch/ApiFecter/index.html?name=Back'
+        window.location.href = 'https://gymgem-5k52v81dh-rimaru06.vercel.app?name=Back'
     });
 }
 const imgdiv1 = document.getElementById('chest');
 if (imgdiv1) {
     imgdiv1.addEventListener('click', function () {
-        window.location.href = 'http://localhost:63342/Fee_Project/fetch/ApiFecter/index.html?name=Chest';
+        window.location.href = 'https://gymgem-5k52v81dh-rimaru06.vercel.app?name=Chest';
     });
 }
 const imgdiv2 = document.getElementById('legs');
 if (imgdiv2) {
     imgdiv2.addEventListener('click', function () {
-        window.location.href = 'http://localhost:63342/Fee_Project/fetch/ApiFecter/index.html?name=Legs';
+        window.location.href = 'https://gymgem-5k52v81dh-rimaru06.vercel.app?name=Legs';
     });
 }
 const imgdiv3 = document.getElementById('bicep');
 if (imgdiv3) {
     imgdiv3.addEventListener('click', function () {
-        window.location.href = 'http://localhost:63342/Fee_Project/fetch/ApiFecter/index.html?name=Bicep';
+        window.location.href = 'https://gymgem-5k52v81dh-rimaru06.vercel.app?name=Bicep';
     });
 }
 const imgdiv4 = document.getElementById('shoulder');
 if (imgdiv4) {
     imgdiv4.addEventListener('click', function () {
-        window.location.href = 'http://localhost:63342/Fee_Project/fetch/ApiFecter/index.html?name=Shoulder';
+        window.location.href = 'https://gymgem-5k52v81dh-rimaru06.vercel.app?name=Shoulder';
     });
 }
 const imgdiv5 = document.getElementById('tricep');
 if (imgdiv5) {
     imgdiv5.addEventListener('click', function () {
-        window.location.href = 'http://localhost:63342/Fee_Project/fetch/ApiFecter/index.html?name=Tricep';
+        window.location.href = 'https://gymgem-5k52v81dh-rimaru06.vercel.app?name=Tricep';
     });
 }
 
